3f723ba085247e7e79563d025f43d82850b4ab7f
kseble <seble.temesgenk@gmail.com>
2023-02-16 10:05:31 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ * main entry
+ * return zero
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
