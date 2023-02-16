0d09be0a4d01116ccd444a6bccdf985fbb46e1d3
kseble <seble.temesgenk@gmail.com>
2023-02-16 10:01:39 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ * main entry point
+ * return: always 0 (success)
+ */
 int main(void)
 {
-	puts("\" Programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multiligual puzzle\n");
 	return (0);
 }
