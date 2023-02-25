23ac1d45aefcb18d73495b8dab67eb6cad7c2c0f
kseble <seble.temesgenk@gmail.com>
2023-02-25 11:49:15 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+
+
 /**
- * main - entry point
- * Description: print a word
- * return 0
+ * main - print _putchar and a new line
+ * 
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
