3e8a2dad84709a1e1748a634db1056398a1d3eec
kseble <kseble>
2023-06-09 10:16:26 -0700

    betty

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -9,21 +9,3 @@ int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
 }
-
-3-puts.c
-
-#include "main.h"
-
-/**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
- */
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
-}
-
