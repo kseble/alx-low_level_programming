33bbbe7df5871de597cf3b470e531155f161a672
seble <seble.temesgenk@gmail.com>
2023-08-22 11:03:02 -0700

    edit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/wish.so https://github.com/kseble/alx-low_level_programming/raw/master/0x18-dynamic_libraries/wish.so
-export LD_PRELOAD=/tmp/wish.so
+wget -O win.so https://github.com/kseble/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=win.so
