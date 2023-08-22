8111a70f33a353e2e97682eafe96dcd3b5cb8921
seble <seble.temesgenk@gmail.com>
2023-08-22 10:41:24 -0700

    edit

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+	return (a - b);
+}
+int mul(int a, int b)
+{
+	return (a * b);
+}
+int divi(int a, int b)
+{
+	return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
